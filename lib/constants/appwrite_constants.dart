94fb25c47b9bdb66f115c7f90a17b3ac3069b938
doshi karan <doshikaran005@gmail.com>
2023-08-06 19:30:41 -0700

    completed authentication

--- lib/constants/appwrite_constants.dart
+++ lib/constants/appwrite_constants.dart
@@ -2,4 +2,6 @@ class AppwriteConstants {
   static const String databaseId = '64cffef8260f71f2123a';
   static const String projectId = '64cffdccf2afb66ccb0f';
   static const String endPoint = 'http://localhost:80/v1';
+
+  static const String usersCollectionId = '64d051062d74b7e2046c';
 }
