4d5dc8af331a9e1734d2d6a6e6db8af43b2cd492
doshi karan <doshikaran005@gmail.com>
2023-08-07 22:29:52 -0700

    profile pic error

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:twitter_clone/common/loading_page.dart';
 import 'package:twitter_clone/features/auth/controller/auth_controller.dart';
-// import 'package:twitter_clone/features/auth/screen/login_screen.dart';
 import 'package:twitter_clone/features/auth/screen/signup_screen.dart';
 import 'package:twitter_clone/features/home/screen/home_screen.dart';
 import 'package:twitter_clone/theme/theme.dart';
 
 import 'common/error_page.dart';
+import 'common/loading_page.dart';
 
 void main() {
-  runApp(const ProviderScope(child: MyApp()));
+  runApp(
+    const ProviderScope(
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends ConsumerWidget {
